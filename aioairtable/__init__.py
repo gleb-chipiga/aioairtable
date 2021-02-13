d49e0af734f7afb71002c88c5e5bec565725d6a0
Gleb Chipiga <gleb.chipiga+git@pm.me>
2021-02-14 01:11:03 +0300

    Export __version__

--- aioairtable/__init__.py
+++ aioairtable/__init__.py
@@ -4,6 +4,6 @@ from .aioairtable import (Airtable, AirtableBase, AirtableRecord,
                           AirtableTable, Attachment, CellFormat, Collaborator,
                           Fields, Method, SortDirection, Thumbnail)
 
-__all__ = ('Method', 'Fields', 'Thumbnail', 'Attachment', 'Collaborator',
-           'SortDirection', 'CellFormat', 'Airtable', 'AirtableBase',
-           'AirtableTable', 'AirtableRecord')
+__all__ = ('__version__', 'Method', 'Fields', 'Thumbnail', 'Attachment',
+           'Collaborator', 'SortDirection', 'CellFormat', 'Airtable',
+           'AirtableBase', 'AirtableTable', 'AirtableRecord')
