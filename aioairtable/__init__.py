7d4c1ef4849f2861df9381c80a6ff743f2a126da
Gleb Chipiga <gleb.chipiga+git@pm.me>
2021-06-19 18:17:55 +0300

    Export RequestAttachment

--- aioairtable/__init__.py
+++ aioairtable/__init__.py
@@ -2,8 +2,10 @@ __version__ = '0.0.12'
 
 from .aioairtable import (DT_FORMAT, Airtable, AirtableBase, AirtableRecord,
                           AirtableTable, Attachment, CellFormat, Collaborator,
-                          Fields, Method, SortDirection, Thumbnail)
+                          Fields, Method, RequestAttachment, SortDirection,
+                          Thumbnail)
 
 __all__ = ('__version__', 'DT_FORMAT', 'Method', 'Fields', 'Thumbnail',
-           'Attachment', 'Collaborator', 'SortDirection', 'CellFormat',
-           'Airtable', 'AirtableBase', 'AirtableTable', 'AirtableRecord')
+           'Attachment', 'RequestAttachment', 'Collaborator', 'SortDirection',
+           'CellFormat', 'Airtable', 'AirtableBase', 'AirtableTable',
+           'AirtableRecord')
--- aioairtable/aioairtable.py
+++ aioairtable/aioairtable.py
@@ -15,8 +15,9 @@ from yarl import URL
 from .helpers import get_software, json_dumps
 
 __all__ = ('DT_FORMAT', 'Method', 'Fields', 'Thumbnail', 'Attachment',
-           'Collaborator', 'SortDirection', 'CellFormat', 'parse_dt',
-           'Airtable', 'AirtableBase', 'AirtableTable', 'AirtableRecord')
+           'RequestAttachment', 'Collaborator', 'SortDirection', 'CellFormat',
+           'parse_dt', 'Airtable', 'AirtableBase', 'AirtableTable',
+           'AirtableRecord')
 
 SOFTWARE: Final[str] = get_software()
 API_URL: Final[URL] = URL('https://api.airtable.com/v0')
