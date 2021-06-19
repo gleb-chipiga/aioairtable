95852675ffb159019b9cae6572e488e8a2e7c50a
Gleb Chipiga <gleb.chipiga+git@pm.me>
2021-06-19 18:18:09 +0300

    Version 0.0.13

--- aioairtable/__init__.py
+++ aioairtable/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.12'
+__version__ = '0.0.13'
 
 from .aioairtable import (DT_FORMAT, Airtable, AirtableBase, AirtableRecord,
                           AirtableTable, Attachment, CellFormat, Collaborator,
